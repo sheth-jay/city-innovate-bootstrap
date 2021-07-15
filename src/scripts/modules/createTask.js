600531c50314f3234f364b19219f7abfdac72b34
mitulsimform <mitul.p@simformsolutions.com>
2021-07-15 20:36:33 +0530

    file name issue has been fixed

--- src/scripts/modules/createTask.js
+++ src/scripts/modules/createTask.js
@@ -1,42 +1,48 @@
-import Api from '../../utils/axios';
+import Api from "../../utils/axios";
 
 $(document).ready(function () {
   let fileList;
 
-  $('#upload_document').change(function (event) {
+  $("#upload_document").change(function (event) {
     const files = event.target.files;
     fileList = {
-      files
+      files,
     };
+    $("#upload_document_label").html(event.target.files[0].name);
   });
 
   $("#createTaskForm").validate({
     submitHandler: function () {
       const formData = new FormData();
-      $("#labels").val().forEach(element => {
-        formData.append('task[label_ids][]', element);
+      $("#labels")
+        .val()
+        .forEach((element) => {
+          formData.append("task[label_ids][]", element);
+        });
+      $("#assignees")
+        .val()
+        .forEach((element) => {
+          formData.append("task[user_ids][]", element);
+        });
+      $("#solicitations")
+        .val()
+        .forEach((element) => {
+          formData.append("task[solicitation_ids][]", element);
+        });
+      Array.from(fileList.files).forEach((file) => {
+        formData.append("task[documents_attributes][]", file);
       });
-      $("#assignees").val().forEach(element => {
-        formData.append('task[user_ids][]', element);
-      });
-      $("#solicitations").val().forEach(element => {
-        formData.append('task[solicitation_ids][]', element);
-      });
-      Array.from(fileList.files).forEach(file => {
-        formData.append('task[documents_attributes][]', file);
-      });
-      
-      formData.append('task[title]', $('#title').val());
-      formData.append('task[description]',$('#description').val());
-      formData.append('task[due_date]', $('#due_date').val());
 
-      Api
-      .post('/tasks', formData, {
+      formData.append("task[title]", $("#title").val());
+      formData.append("task[description]", $("#description").val());
+      formData.append("task[due_date]", $("#due_date").val());
+
+      Api.post("/tasks", formData, {
         headers: {
-          'Content-Type': 'multipart/form-data'
-        }
+          "Content-Type": "multipart/form-data",
+        },
       })
-      .then(function () {
+        .then(function () {
           window.location.href = "/";
         })
         .catch(function (error) {
@@ -46,4 +52,3 @@ $(document).ready(function () {
     },
   });
 });
-
