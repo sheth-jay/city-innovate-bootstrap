727998b5261fb2cb06b254b79fbc2f8cfd8ad66b
sheth-jay <jay.sheth@simformsolutions.com>
2021-07-15 15:31:18 +0530

    completed login and signup api integration

--- src/scripts/modules/login.js
+++ src/scripts/modules/login.js
@@ -2,31 +2,21 @@ import axios from "axios";
 import { BaseAPIURL } from "../../utils/config";
 
 $(document).ready(function () {
-  //   $("#commentForm").submit(function (event) {
-  //     event.preventDefault();
-  //     $("#commentForm").validate();
-  //     console.log("evebt", $("#commentForm").validate());
-  //     alert("Submitted");
-  //   });
-  $("#commentForm").validate({
+  $("#loginForm").validate({
     submitHandler: function () {
-      // some other code
-      // maybe disabling submit button
-      // then:
       axios
-        .post(`${BaseAPIURL}/sign_in`, {
-          user: { email: "mitul@gmail.com", password: 123456 },
-        })
-        .then(function (response) {
+      .post(`${BaseAPIURL}/sign_in`, {
+        user: { email: $('#email').val(), password: $('#password').val() },
+      })
+      .then(function (response) {
           localStorage.setItem("token", response.data.data.auth_token);
           window.location.href = "/";
         })
         .catch(function (error) {
-          window.location.href = "/login";
+          window.location.href = "/login.html";
           throw error;
         });
-      alert("here");
-      //   $(form).submit();
     },
   });
 });
+
